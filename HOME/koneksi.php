d8f4d85d1c03fc4c9dd9fd8c601ec473e091ac2e
Mabrur Syamhur <56716500+imboyo@users.noreply.github.com>
2025-05-22 00:16:21 +0800

    Konsek

--- HOME/koneksi.php
+++ HOME/koneksi.php
@@ -1,13 +1,13 @@
 <?php 
-// $host = "127.0.0.1";
-// $user = "mostyjpc_root";
-// $pass = "!RC@~LxudH8.";
-// $db = "mostyjpc_aplikasi-arsip";
-
 $host = "127.0.0.1";
-$user = "root";
-$pass = "";
-$db = "dbsurat";
+$user = "mostyjpc_root";
+$pass = "!RC@~LxudH8.";
+$db = "mostyjpc_aplikasi-arsip";
+
+// $host = "127.0.0.1";
+// $user = "root";
+// $pass = "";
+// $db = "dbsurat";
 
 $koneksi = new mysqli($host, $user, $pass, $db);
 ?>
